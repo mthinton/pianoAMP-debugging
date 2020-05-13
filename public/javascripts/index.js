062051bbdaa5288706e01a48cf2bee21c839cfd5
Matt <matt.hinton@piano.io>
2020-05-13 09:30:07 -0400

    added login/logout buttons

---
+++ public/javascripts/index.js
@@ -0,0 +1,1 @@
+(function(src){var a=document.createElement("script");a.type="text/javascript";a.async=true;a.src=src;var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b)})("https://sandbox.tinypass.com/xbuilder/experience/load?aid=bW69IEsHwA")
